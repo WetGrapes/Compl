e25082c5f237d48879341410bf17ac16a95718b6
Boris <boriswp99@gmail.com>
2021-05-31 23:16:07 +0500

    next

--- Assets/Translation/Analyzer.cs
+++ Assets/Translation/Analyzer.cs
@@ -8,23 +8,21 @@ using UnityEngine;
 
 public class Analyzer : MonoBehaviour
 {
-    private string[] Words = { "program", "var", "integer", "real", "bool", "begin", "end", "if", "then", "else", "while", "do", "read", "write", "true", "false" };
-    private string[] Delimiter = { ".", ";", ",", "(", ")", "+", "-", "*", "/", "=", ">", "<" };
-  
     public List<Lex> Lexemes = new List<Lex>();
-
+    public TextMeshProUGUI output;
     private string buf = ""; // буфер для хранения лексемы
     private string prevBuf = ""; // буфер для хранения прошлой лексемы
     private char[] sm = new char[1];
-    private int dt = 0, mantis=1;
+    private int dt = 0, mantis = 1;
     private float fl = 0;
     // состояния state-машины
-    private States state = States.S; // хранит текущее состояние
+    private States globalState = States.S; // хранит текущее состояние
+    private States localState = States.S; // хранит текущее состояние
     private StringReader sr; // позволяет посимвольно считывать строку
     private string[] TNUM;
     private string[] TID;
     private int tics = 0;
-    
+
     private Dictionary<int, string> typesOfLexem = new Dictionary<int, string>()
     {
         {1, "служебные слова"},
@@ -35,41 +33,42 @@ public class Analyzer : MonoBehaviour
     };
 
     public TMP_InputField Input;
-    public void StartAnalyze(){
+    public void StartAnalyze()
+    {
         Lexemes = new List<Lex>();
-        TNUM = new string[]{};
-        TID = new string[]{};
-        state = States.S;
+        TNUM = new string[] { };
+        TID = new string[] { };
+        globalState = States.S;
         buf = "";
         dt = 0;
-        
+
         Analysis(Input.text);
-        
+
         foreach (var lexeme in Lexemes) Debug.Log(lexeme.val + "\t" + typesOfLexem[lexeme.id]);
         Debug.Log("F");
     }
-   
+
 
     private (int, string) PushLex(string[] lexes, string buf)
     {
         lexes ??= new string[] { };
         var srh = Array.FindIndex(lexes, s => s.Equals(buf));
         if (srh != -1) return (-1, "");
-        
+
         Array.Resize(ref lexes, lexes.Length + 1);
         lexes[lexes.Length - 1] = buf;
         return (lexes.Length - 1, buf);
-        
+
     }
     public void Analysis(string text)
     {
-        if(text == null || text.Length <= 0) return;
+        if (text == null || text.Length <= 0) return;
         sr = new StringReader(text);
-        tics = text.Length*2;
-        while (state != States.F && tics>0)
+        tics = text.Length * 2;
+        while (globalState != States.F && tics > 0)
         {
             tics--;
-            switch (state)
+            switch (globalState)
             {
 
                 case States.S:
@@ -79,24 +78,19 @@ public class Analyzer : MonoBehaviour
                     {
                         buf = "";
                         buf += sm[0];
-                        state = States.ID;
+                        globalState = States.ID;
                         GetNext();
                     }
                     else if (char.IsDigit(sm[0]))
                     {
                         dt = (int)(sm[0] - '0');
                         GetNext();
-                        state = States.INT;
+                        globalState = States.INT;
 
-                    }
-                    else if (sm[0] == '.')
-                    {
-                        Lexemes.Add(new Lex(2, 0, sm[0].ToString()));
-                        state = States.F;
                     }
                     else
                     {
-                        state = States.DLM;
+                        globalState = States.DLM;
                     }
 
                     break;
@@ -112,14 +106,16 @@ public class Analyzer : MonoBehaviour
                         if (srch.Item1 != -1)
                         {
                             ///если нашли стандартное слово
-                            state = (States) srch.Item1;
+                            globalState = (States)srch.Item1;
+                            Lexemes.Add(new Lex(1, srch.Item1, srch.Item2));
                         }
                         else
                         {
                             var j = PushLex(TID, buf);
                             Lexemes.Add(new Lex(4, j.Item1, j.Item2));
+
                         }
-                        state = States.S;
+                        globalState = States.S;
                     }
                     break;
 
@@ -132,57 +128,72 @@ public class Analyzer : MonoBehaviour
                     else if (sm[0] == '.' || sm[0] == ',')
                     {
                         fl = dt;
-                        state = States.FLOAT;
+                        globalState = States.FLOAT;
                         GetNext();
-                    } else
+                    }
+                    else
                     {
                         var (item1, item2) = PushLex(TNUM, dt.ToString());
                         Lexemes.Add(new Lex(3, item1, item2));
-                        state = States.S;
+                        globalState = States.S;
                     }
                     break;
                 case States.FLOAT:
                     if (char.IsDigit(sm[0]))
                     {
-                        fl += (float)(sm[0] - '0')/Mathf.Pow(10,mantis++);
+                        fl += (float)(sm[0] - '0') / Mathf.Pow(10, mantis++);
                         GetNext();
-                    }else
+                    }
+                    else
                     {
                         mantis = 1;
                         var (a, b) = PushLex(TNUM, dt.ToString());
                         Lexemes.Add(new Lex(3, a, b));
-                        state = States.S;
+                        globalState = States.S;
                     }
                     break;
                 case States.DLM:
-                    
+
                     buf = "";
                     buf += sm[0];
 
-                    var (c, d) = WordTable.SearchLex(Delimiter, buf);
+                    var (c, d) = WordTable.StateSearch(States.DLM, buf);
                     if (c != -1)
                     {
                         Lexemes.Add(new Lex(2, c, d));
-                        state = States.S;
+                        globalState = States.S;
                         GetNext();
                     }
                     else
-                        state = States.ER;
+                        globalState = States.ER;
                     break;
                 case States.ER:
                     Debug.Log("Ошибка в программе");
-                    state = States.F;
+                    globalState = States.F;
                     break;
                 case States.F:
                     Debug.Log("Лексический анализ закончен");
                     break;
+
             }
+        }
+        Output();
+    }
+    private void Output()
+    {
+        foreach (var lex in Lexemes)
+        {
+            output.text += lex.val + " ";
         }
     }
     private void GetNext()
     {
         var res = sr.Read(sm, 0, 1);
-        if (res == -1) sm[0] = '.';
+        if (res == -1)
+        {
+            Lexemes.Add(new Lex(2, (int)States.DLM, "}"));
+            globalState = States.F;
+        }
     }
 
 }
--- Assets/Translation/WordTable.cs
+++ Assets/Translation/WordTable.cs
@@ -4,18 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-public enum States { S = 0, INT, FLOAT, F, ID, ER, CON, FUNC, TYPE, LOOPFOR, WORD, DLM }
+public enum States { S = 0, INT, FLOAT, F, ID, ER, CON, FUNC, TYPE, LOOPFOR, WORD, DLM, NL }
 
 public class WordTable : MonoBehaviour
 {
-    public static (int, string) WideSearch( string buf)
+    public static (int, string) WideSearch(string buf)
     {
         for (var i = startIndex; i < count; i++)
         {
-            var srch = StateSearch((States) i, buf);
+            var srch = StateSearch((States)i, buf);
             if (srch.Item1 != -1) return srch;
         }
-        return (-1,"");
+        return (-1, "");
     }
     public static (int, string) StateSearch(States _state, string buf)
     {
@@ -25,23 +25,30 @@ public class WordTable : MonoBehaviour
         {
             (a, b) = SearchLex(list.Value.ToArray(), buf);
             if (a == -1) continue;
-            a = (int) _state;
+            a = (int)_state;
             b = list.Key;
             break;
         }
         return (a, b);
     }
-    public static  (int, string) SearchLex(string[] lexes, string buf)
+    public static (int, string) SearchLex(string[] lexes, string buf)
     {
-        var srh = Array.FindIndex(lexes, s => s.Equals(buf));
+        var srh = Array.FindIndex(lexes, s => 
+        {
+            if (buf.StartsWith(s))
+            {
+                return true;
+            }
+            else return false;
+        });
         return srh != -1 ? (srh, buf) : (-1, "");
     }
-    
+
     static int count = Enum.GetNames(typeof(States)).Length;
     private static int startIndex = 6;
-    
-    public static Dictionary<States, Dictionary<string,List<string>>> val 
-        = new Dictionary<States, Dictionary<string,List<string>>>
+
+    public static Dictionary<States, Dictionary<string, List<string>>> val
+        = new Dictionary<States, Dictionary<string, List<string>>>
     {
         {
         States.CON, new Dictionary<string,List<string>>
@@ -53,60 +60,66 @@ public class WordTable : MonoBehaviour
     {
         States.FUNC, new Dictionary<string,List<string>>
         {
-            {"",new List<string>{"функция", "подпрограмма", "вызвать", "вызовем", "вызываем"}},
-            {")\n{",new List<string>{"которая"}},
+            {"",new List<string>{"функц", "подпрограмм", "вызва", "вызов", "вызыв","парамет"}},
+            {"\n{",new List<string>{"котор"}},
+            {")",new List<string>{";"} }
         }
     },
     {
         States.TYPE, new Dictionary<string,List<string>>
         {
-            {"int",new List<string>{"целое", "целочис", "целочисленное"}},
-            {"float",new List<string>{"дробное", "веществ", "вещественное"}},
-            {"char",new List<string>{"символ", "символьная", "символьный"}},
-            {"bool",new List<string>{"логическая", "логич"}},
+            {"int",new List<string>{"целоч","цела"}},
+            {"float",new List<string>{"дробн", "веществ"}},
+            {"char",new List<string>{"символ"}},
+            {"bool",new List<string>{"логич"}},
         }
     },
     {
         States.LOOPFOR, new Dictionary<string,List<string>>
         {
             {"for(",new List<string>{"для"}},
-            {";",new List<string>{",", "веществ", "вещественное"}},
+            {";",new List<string>{",", "веществ"}},
             {"", new List<string>{"пока", "до","тех", "пор"}},
-            {")\n{",new List<string>{"выполнить", "сделать"}},
-            {"break", new List<string>{"остановить"}},
-            {"continue", new List<string>{"следующий"}},
+            {")\n{",new List<string>{"выполн", "сдела"}},
+            {"break", new List<string>{"останови"}},
+            {"continue", new List<string>{"следующ"}},
         }
     },
     {
         States.WORD, new Dictionary<string,List<string>>
         {
-            {"break", new List<string>{"остановить"}},
-            {"continue", new List<string>{"следующий"}},
-            {"return", new List<string>{"вернуть", "возвращает"}},
+            {"break", new List<string>{"останови"}},
+            {"continue", new List<string>{"следующ"}},
+            {"return", new List<string>{"вернуть", "возвра"}},
         }
     },
     {
         States.DLM, new Dictionary<string,List<string>>
         {
             {"1", new List<string>{"один"}},
-            {"+",new List<string>{"сложить", "прибавить","увеличить", "увеличивает", "плюс"}}, 
-            {"-",new List<string>{"уменьшить", "уменьшает"}},
-            {"*",new List<string>{"умножить", "умножаем"}},
-            {"/",new List<string>{"разделить", "делит"}},
-            {">",new List<string>{"больше"}}, 
+            {"+",new List<string>{"слож", "прибав","увеличи", "плюс"}},
+            {"-",new List<string>{"уменьш","минус"}},
+            {"*",new List<string>{"умнож"}},
+            {"/",new List<string>{"раздел", "делит"}},
+            {">",new List<string>{"больше"}},
             {"<",new List<string>{"меньше"}},
-            {"==",new List<string>{"эквивалентно", "эквивалентен"}},
+            {"==",new List<string>{"эквивалент"}},
             {"&&",new List<string>{"и"}},
             {"||",new List<string>{"или"}},
             {"!",new List<string>{"не"}},
-            {"=",new List<string>{"равно", "равняется", "равном"}},
-            {")\n{",new List<string>{"то", "тогда", "которая"}},
-            {"}", new List<string>{".", "..."}},
+            {"=",new List<string>{"равн"}},
+            {")\n{",new List<string>{"то", "тогда", "котор"}},
+            {"}", new List<string>{"."}},
             {";", new List<string>{";"}},
-            {"(",new List<string>{"от", "при", "с", ":"}},
-            {";\n",new List<string>{"\n"}},
+            {"(",new List<string>{"от", "при", "с", ":","без"}},
             {",",new List<string>{","}},
             {"\"",new List<string>{"\""}},
+        }
+    },
+    { 
+        States.NL, new Dictionary<string,List<string>>
+        {
+            {";\n",new List<string>{"\n"}},
         }
     },
     };
